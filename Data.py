d5c7e7141fa51bf3f1836d845f9e1a5c1bc2f21c
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-04 01:05:27 +0530

    Create Data.py

---
+++ Data.py
@@ -0,0 +1,67 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+class Data:
+    # Start Message
+    START = """
+Hey {}
+
+Welcome to {}
+
+If you don't trust this bot, 
+1) stop reading this message
+2) delete this chat
+
+Still reading?
+You can use me to generate pyrogram and telethon string session. Use below buttons to learn more !
+
+✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!
+    """
+
+    # Home Button
+    home_buttons = [
+        [InlineKeyboardButton("💟 Start Generating Session 💟", callback_data="generate")],
+        [InlineKeyboardButton(text="🚸 Return Home 🚸", callback_data="home")]
+    ]
+
+    generate_button = [
+        [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")]
+    ]
+
+    # Rest Buttons
+    buttons = [
+        [InlineKeyboardButton("🎉 Start Generating Session 🎉", callback_data="generate")],
+        [InlineKeyboardButton("💕 Bot Status and More Bots 💕", url="https://t.me/DeeCodeBots/32")],
+        [
+            InlineKeyboardButton("How to Use 🤭❔", callback_data="help"),
+            InlineKeyboardButton("😏 About 😏", callback_data="about")
+        ],
+        [InlineKeyboardButton("🥱 More Amazing bots 🥱", url="https://t.me/DeeCodeBots")],
+    ]
+
+    # Help Message
+    HELP = """
+✨ **Available Commands** ✨
+
+/about - About The Bot
+/help - This Message
+/start - Start the Bot
+/generate - Start Generating Session
+/cancel - Cancel the process
+/restart - Cancel the process
+"""
+
+    # About Message
+    ABOUT = """
+**About This Bot** 
+
+A telegram bot to generate pyrogram and telethon string session by @StarkBots
+
+Source Code : [Click Here](https://github.com/AMANTYA1/String_gen)
+
+Framework : [Pyrogram](docs.pyrogram.org)
+
+Language : [Python](www.python.org)
+
+Developer : @TeamDeeCode
+    """
